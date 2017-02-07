75a73b4c24b808c751ba6d4e0d38d1914a414e76
Jan Benes <jan.benes@deutsche-boerse.com>
2017-02-07 09:55:23 +0100

    odendany ifdefy pro COMPILE_AVR a trochu zmeneny makefile

--- makefile
+++ makefile
@@ -3,12 +3,13 @@ PROJECT = pivotoc
 MCU = atmega328p
 PRIPONA = .elf
 TARGET = pivotoc.elf
-CESTA=/usr/local/CrossPack-AVR/avr/bin/
-#CESTA="C:/WinAVR-20100110\\bin\\"
-#CESTA =
-CC = $(CESTA)avr-gcc
-#CC = $(CESTA)avr-c++
 
+#CESTA=/usr/local/CrossPack-AVR/avr/bin/
+#CESTA="C:/WinAVR-20100110\\bin\\"
+CESTA =
+
+#CC = $(CESTA)avr-gcc
+CC = $(CESTA)avr-c++
 CPP = $(CESTA)avr-c++
 
 ## Options common to compile, link and assembly rules
@@ -16,8 +17,14 @@ COMMON = -mmcu=$(MCU)
 
 ## Compile options common for all C compilation units.
 CFLAGS = $(COMMON)
-CFLAGS += -Wall -gdwarf-2 -DF_CPU=20000000UL -O1 -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums
-CFLAGS += -MD -MP -MT $(*F).o -MF dep/$(@F).d -fdata-sections -ffunction-sections
+CFLAGS += -ffreestanding
+CFLAGS += -fno-tree-scev-cprop
+CFLAGS += -mcall-prologues 
+CFLAGS += -fno-jump-tables
+CFLAGS += -std=gnu99 -Wall -gdwarf-2 -g -DF_CPU=16000000UL -O1 
+CFLAGS += -funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums
+CFLAGS += -fdata-sections -ffunction-sections
+#CFLAGS += -MD -MP -MT $(*F).o -MF dep/$(@F).d 
 
 ## Assembly specific flags
 ASMFLAGS = $(COMMON)
@@ -25,7 +32,7 @@ ASMFLAGS += $(CFLAGS)
 ASMFLAGS += -x assembler-with-cpp -Wa,-gdwarf2
 
 ## Linker flags
-LDFLAGS = $(COMMON) -dead_strip -Wl,--gc-sections 
+LDFLAGS = $(COMMON) -Wl,--gc-sections 
 LDFLAGS +=  -Wl,-Map=pivotoc.map
 
 ## Intel Hex file production flags
@@ -37,17 +44,19 @@ HEX_EEPROM_FLAGS += --change-section-lma .eeprom=0 --no-change-warnings
 
 ## Include Directories
 #INCLUDES = -I"C:/WinAVR-20100110/avr/include" -I"C:/Honza/_nrf24l01"
-INCLUDES = -I"/usr/local/CrossPack-AVR/avr/include/"
+#INCLUDES = -I"/usr/local/CrossPack-AVR/avr/include/"
+INCLUDES = 
 
 ## Library Directories
 #LIBDIRS = -L"C:/WinAVR-20100110/avr/lib" 
-LIBDIRS = -L"/usr/local/CrossPack-AVR/avr/lib/"
+#LIBDIRS = -L"/usr/local/CrossPack-AVR/avr/lib/"
+LIBDIRS = 
 
 ## Objects that must be built in order to link
 OBJECTS = pivotoc_main.o
 
 ## Objects explicitly added by the user
-LIBS = 
+LIBS = -lprintf_flt -lm
 
 ## Build
 all: clean $(TARGET) pivotoc.hex pivotoc.eep pivotoc.lss avr-size
@@ -82,5 +91,5 @@ clean:
 
 
 ## Other dependencies
--include $(shell mkdir dep 2>NUL) $(wildcard dep/*)
+#-include $(shell mkdir dep 2>NUL) $(wildcard dep/*)
 
--- pivotoc_main.cpp
+++ pivotoc_main.cpp
@@ -1,4 +1,3 @@
-#ifdef COMPILE_AVR
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h> 
@@ -6,30 +5,24 @@
 #include <avr/eeprom.h>
 
 #include "onewire.h"
-#else
-typedef unsigned char uint8_t;
-typedef unsigned short int uint16_t;
-typedef unsigned int uint32_t;
-#endif
-
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
 
 typedef union {
-  uint16_t uint;
-  struct {
-    uint8_t lsb;
-    uint8_t msb;
-  };
+	uint16_t uint;
+	struct {
+		uint8_t lsb;
+		uint8_t msb;
+	};
 } IntUnion_t;
 
 typedef union {
- uint32_t uint_long;
- struct {
-	IntUnion_t uint1;
-	IntUnion_t uint2;
- };
+	uint32_t uint_long;
+	struct {
+		IntUnion_t uint1;
+		IntUnion_t uint2;
+	};
 } Int32Union_t;
 
 #define ADRESA_EE_CENA_PIVA 0
@@ -45,8 +38,17 @@ typedef union {
 #define PIN_STAV_SPRAVA 6
 #define PORT_STAV PORTB
 
+#define BRANA_SOLENOID D
 #define PIN_SOLENOID 5
-#define PORT_SOLENOID PORTD
+
+#define XSTR(X,Y) X ## Y
+#define PORT_BUILD(X) XSTR(PORT,X)
+#define DDR_BUILD(X) XSTR(DDR,X)
+
+#define PORT_SOLENOID PORT_BUILD(BRANA_SOLENOID)
+#define DDR_SOLENOID DDR_BUILD(BRANA_SOLENOID)
+
+#define SOLENOID_TO_OUTPUT DDR_SOLENOID |= (1 << PIN_SOLENOID)
 #define SOLENOID_OFF() PORT_SOLENOID &= ~(1 << PIN_SOLENOID)
 #define SOLENOID_ON() PORT_SOLENOID |= (1 << PIN_SOLENOID)
 
@@ -68,7 +70,7 @@ const uint8_t ADRESY_CIPU[][ADDR_LEN] = { {0x1,0x2,0x3,0x4,0x5,0x6,0x7,0x8}, {0x
 #define POCET_CIPU sizeof(ADRESY_CIPU) / sizeof(*ADRESY_CIPU)
 volatile uint8_t  KONTROLNI_SOUCTY[POCET_CIPU]; //kontrolni soucty adres pro rychlejsi vyhledavani
 
-volatile uint16_t VYTOCENE_IMPULZY[POCET_CIPU]; //aktualne vytocene impulzy od posledni zmeny ceny piva (nebo od zacatku)
+volatile uint16_t AKTUALNI_IMPULZY[POCET_CIPU]; //aktualne vytocene impulzy od posledni zmeny ceny piva (nebo od zacatku)
 //sem se presouvaji dosavadni impulzy a spocita se dosavadni cena za ty impulzy, pokud se zmeni cena piva
 volatile uint16_t AKUMULOVANE_IMPULZY[POCET_CIPU];
 volatile uint16_t AKUMULOVANA_CENA[POCET_CIPU]; //cena je v halirich!!
@@ -84,7 +86,7 @@ volatile uint8_t  prihlaseny_cip_adresa[ADDR_LEN];
 volatile uint16_t prihlaseny_cip_impulzy;
 volatile uint16_t prihlaseny_cip_timeout;
 
-volatile uint16_t longTimer;
+//volatile uint16_t longTimer;
 volatile uint8_t  timerCteniCipu;
 volatile uint8_t  bylTimer;
 
@@ -103,7 +105,7 @@ volatile uint8_t timerUart;
 volatile uint8_t  refresh_display;
 
 enum MozneStavyDispleje {
-	STAV_DISPLEJE_NIC = 0,
+	DISP_STAV_NIC = 0,
 	DISP_STAV_ZAKLADNI,
 	DISP_STAV_ZAKLADNI_SPRAVA,
 	DISP_STAV_INICIALIZACE,
@@ -114,7 +116,7 @@ enum MozneStavyDispleje {
 };
 
 #define DISPLAY_FRONTA_MAXLEN 5
-#define DISPLAY_REFRESH_TIME TIMER_1SEC
+#define DISPLAY_REFRESH_TIME TIMER_1SEC*2
 volatile uint8_t display_fronta[DISPLAY_FRONTA_MAXLEN];
 volatile uint8_t display_fronta_len;
 volatile uint8_t display_posledni_stav;
@@ -126,12 +128,28 @@ volatile char displej_text[DISP_SIZE];
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-void DisplayFrontaPush(uint8_t novy_stav)
+void DisplayFrontaAdd(uint8_t novy_stav)
 {
 	//pridame stav na konec pole (doufam ze nenastane pripad, aby se pole nekdy preplnilo)
 	//to pak nevim jestli je lepci diskardovat prvni a nebo posledni polozku
 	//ted se holt kdyztak nahradi dycky ta posledni
 	display_fronta[display_fronta_len] = novy_stav;
+	display_fronta_len++;
+	if (display_fronta_len == DISPLAY_FRONTA_MAXLEN) display_fronta_len--;
+}
+
+//======================================================
+void DisplayFrontaPush(uint8_t novy_stav)
+{
+	//vlozime stav na uplny zacatek pole - bude pri refreshi hned zobrazen (prednostni funkce)
+
+	//posuneme vsechny polozky, ale musime jit odzadu
+	for (uint8_t i = display_fronta_len; i > 0; i--)
+	{
+		display_fronta[i] = display_fronta[i-1];
+	}
+
+	display_fronta[0] = novy_stav;
 	display_fronta_len++;
 	if (display_fronta_len == DISPLAY_FRONTA_MAXLEN) display_fronta_len--;
 }
@@ -143,7 +161,7 @@ uint8_t DisplayFrontaPop(void)
 	//a nakonec jednu nulu vlozime
 
 	//asi muzem rict, ze kdyz je pole prazdne, vracime automaticky stav NIC
-	if (display_fronta_len == 0) return STAV_DISPLEJE_NIC;
+	if (display_fronta_len == 0) return DISP_STAV_NIC;
 
 	uint8_t vratit = display_fronta[0];
 	for (uint8_t i=0; i < display_fronta_len; i++)
@@ -151,7 +169,7 @@ uint8_t DisplayFrontaPop(void)
 		display_fronta[i] = display_fronta[i+1];
 	}
 
-	display_fronta[display_fronta_len] = STAV_DISPLEJE_NIC;
+	display_fronta[display_fronta_len] = DISP_STAV_NIC;
 	display_fronta_len--;
 
 	return vratit;
@@ -164,37 +182,36 @@ void PrekreslitDisplay(uint8_t novy_stav)
 }
 
 //======================================================
-#ifdef COMPILE_AVR
 void SetRegisters(void)
 {
-  //configure uart0  (57600, 8bits, no parity, 1 stop bit)
-  UBRR0H = 0;
-  UBRR0L = 16;
-  UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-  UCSR0B = _BV(RXEN0) | _BV(TXEN0);
-  
-  //timer0 10ms period, interrupt enable
-  //prescaler 1024, count to 156
-  OCR0A = 156;
-  OCR0B = 170;
-  TCCR0A = 2;
-  TCCR0B = 5;
-  TIMSK0 = 2;
+	//configure uart0  (57600, 8bits, no parity, 1 stop bit)
+	UBRR0H = 0;
+	UBRR0L = 16;
+	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
+	UCSR0B = _BV(RXEN0) | _BV(TXEN0);
 
-  //disable unused peripherials
-  //PRR = ( _BV(PRTWI) | _BV(PRTIM1) | _BV(PRTIM2) ) ;
+	//timer0 10ms period, interrupt enable
+	//prescaler 1024, count to 156
+	OCR0A = 156;
+	OCR0B = 170;
+	TCCR0A = 2;
+	TCCR0B = 5;
+	TIMSK0 = 2;
+
+	//disable unused peripherials
+	//PRR = ( _BV(PRTWI) | _BV(PRTIM1) | _BV(PRTIM2) ) ;
 }
 
 //======================================================
 void USART_Transmit( char *data, uint8_t len )
 {
-  for (uint8_t i=0; i < len; i++)
-  {
-    /* Wait for empty transmit buffer */
-    while ( !( UCSR0A & (1<<UDRE0)) );
-    /* Put data into buffer, sends the data */
-    UDR0 = data[i];
-  }
+	for (uint8_t i=0; i < len; i++)
+	{
+		/* Wait for empty transmit buffer */
+		while ( !( UCSR0A & (1<<UDRE0)) );
+		/* Put data into buffer, sends the data */
+		UDR0 = data[i];
+	}
 }
 
 //======================================================
@@ -229,12 +246,12 @@ ISR(USART_RX_vect)
 //======================================================
 ISR(TIMER0_COMPA_vect)
 {
-  	longTimer++;
-		bylTimer = true;
+	//longTimer++;
+	bylTimer = true;
 }
 
 ISR(BADISR_vect) { //just for case
-  __asm__("nop\n\t");
+	__asm__("nop\n\t");
 }
 
 
@@ -250,7 +267,7 @@ void PosliDataNaUart(void)
 
 	for (uint8_t cip=0; cip < POCET_CIPU; cip++)
 	{
-		uni.uint = VYTOCENE_IMPULZY[cip] + AKUMULOVANE_IMPULZY[cip];
+		uni.uint = AKTUALNI_IMPULZY[cip] + AKUMULOVANE_IMPULZY[cip];
 		sendBuff[0] = uni.lsb;
 		sendBuff[1] = uni.msb;
 		p16 = (IntUnion_t*)&AKUMULOVANA_CENA[cip];
@@ -261,12 +278,11 @@ void PosliDataNaUart(void)
 	}
 }
 
-#endif
 
 //======================================================
 void VynulujCip(uint8_t id)
 {
-	VYTOCENE_IMPULZY[id] = 0;
+	AKTUALNI_IMPULZY[id] = 0;
 	AKUMULOVANE_IMPULZY[id] = 0;
 	AKUMULOVANA_CENA[id] = 0;
 }
@@ -285,10 +301,10 @@ void ResetujVsechnyCipy(void)
 //======================================================
 void AkumulujCenu(uint8_t cip)
 {
-	uint16_t cena = VYTOCENE_IMPULZY[cip] * CENA_ZA_IMPULZ * 100; //je to na halire
+	uint16_t cena = AKTUALNI_IMPULZY[cip] * CENA_ZA_IMPULZ * 100; //je to na halire
 	AKUMULOVANA_CENA[cip] += cena;
-	AKUMULOVANE_IMPULZY[cip] += VYTOCENE_IMPULZY[cip];
-	VYTOCENE_IMPULZY[cip] = 0;
+	AKUMULOVANE_IMPULZY[cip] += AKTUALNI_IMPULZY[cip];
+	AKTUALNI_IMPULZY[cip] = 0;
 }
 
 //======================================================
@@ -314,7 +330,6 @@ void ZmenCenu(uint16_t nova_cena)
 //======================================================
 void SaveData(void)
 {
-#ifdef COMPILE_AVR
 	IntUnion_t volatile *p16;
 	Int32Union_t volatile eep_dword;
 
@@ -345,13 +360,11 @@ void SaveData(void)
 
 		adresa += 4; //posun se na dalsi pametove misto
 	}
-#endif
 }
 
 //======================================================
 void LoadData(void)
 {
-#ifdef COMPILE_AVR
 	IntUnion_t volatile *p16;
 	Int32Union_t volatile eep_dword;
 
@@ -391,17 +404,9 @@ void LoadData(void)
 
 		adresa += 4; //posun se na dalsi pametove misto
 		
-		VYTOCENE_IMPULZY[cip] = 0;
+		AKTUALNI_IMPULZY[cip] = 0;
 		CELKOVE_IMPULZY += AKUMULOVANE_IMPULZY[cip];
 	}
-#else
-
-	CELKOVE_IMPULZY = 3000;
-	//IMPULZY_NA_LITR = 300;
-	IntUnion_t volatile *p16 = (IntUnion_t*)&IMPULZY_NA_LITR;
-	(*p16).uint = 300;
-	CENA_PIVA = 20;
-#endif
 
 	//cena piva na impulz
 	CENA_ZA_IMPULZ = float(2 * CENA_PIVA) / IMPULZY_NA_LITR;
@@ -470,8 +475,8 @@ uint8_t NajdiCip(const uint8_t adresa[ADDR_LEN])
 //======================================================
 void ZobrazInfoCipSprava(uint8_t id)
 {
-	float litru = float(AKUMULOVANE_IMPULZY[id] + VYTOCENE_IMPULZY[id]) / IMPULZY_NA_LITR;
-	uint16_t cena = (VYTOCENE_IMPULZY[id] * CENA_ZA_IMPULZ * 100) + AKUMULOVANA_CENA[id]; //je to na halire
+	float litru = float(AKUMULOVANE_IMPULZY[id] + AKTUALNI_IMPULZY[id]) / IMPULZY_NA_LITR;
+	uint16_t cena = (AKTUALNI_IMPULZY[id] * CENA_ZA_IMPULZ * 100) + AKUMULOVANA_CENA[id]; //je to na halire
 
 	//cena total je v korunach a zaokrouhluje se nahoru
 	uint16_t cena_total = (cena / 100);
@@ -483,7 +488,7 @@ void ZobrazInfoCipSprava(uint8_t id)
 //======================================================
 void ZobrazInfoCipVytoc(uint8_t id, bool both)
 {
-	float litru = float(AKUMULOVANE_IMPULZY[id] + VYTOCENE_IMPULZY[id]) / IMPULZY_NA_LITR;
+	float litru = float(AKUMULOVANE_IMPULZY[id] + AKTUALNI_IMPULZY[id]) / IMPULZY_NA_LITR;
 	float nyni = float(prihlaseny_cip_impulzy) / IMPULZY_NA_LITR;
 
 	if (both == true)
@@ -502,11 +507,9 @@ void OdhlasCip(void)
 	prihlaseny_cip_timeout = 0;
 	prihlaseny_cip_impulzy = 0;
 	//sprintf((char *)displej_text, SCREEN_ZAKLADNI);
-	DisplayFrontaPush(DISP_STAV_ZAKLADNI);
+	DisplayFrontaAdd(DISP_STAV_ZAKLADNI);
 	refresh_display = true;
-#ifdef COMPILE_AVR
 	SOLENOID_OFF();
-#endif
 }
 
 //======================================================
@@ -515,7 +518,6 @@ void OdhlasCip(void)
 //pak zkusi cip najit a pripadne prihlasit
 void PrectiCip(void)
 {
-#ifdef COMPILE_AVR
 
 	if (ow_rom_search(OW_SEARCH_FIRST, (uint8_t *)prihlaseny_cip_adresa) == OW_LAST_DEVICE)
 	{
@@ -550,7 +552,6 @@ void PrectiCip(void)
 		refresh_display = true;
 	}
 
-#endif
 }
 
 //======================================================
@@ -562,24 +563,21 @@ int main (void)
 
 	LoadData();
 	SpocitatKontrolniSoucty();
-
-#ifdef COMPILE_AVR
 	SetRegisters();
 	sei();
-#endif
 
 	aktualni_stav = STAV_NORMAL;
 	display_posledni_stav = DISP_STAV_INICIALIZACE;
 	//sprintf((char *)displej_text, SCREEN_ZAKLADNI);
-	DisplayFrontaPush(DISP_STAV_ZAKLADNI);	
+	DisplayFrontaAdd(DISP_STAV_ZAKLADNI);	
 	refresh_display = true;
 
-
+#ifdef DEBUG_ON_PC
 	//debug data
 	uint8_t zak = 0;
 	AKUMULOVANE_IMPULZY[zak] = 0;
 	AKUMULOVANA_CENA[zak] = 0;
-	VYTOCENE_IMPULZY[zak] = 388;
+	AKTUALNI_IMPULZY[zak] = 388;
 	prihlaseny_cip_impulzy = 132;
 
 	printf("\n|--------|---------|\n");
@@ -590,7 +588,7 @@ int main (void)
 
 	ZmenCenu(40);
 
-	VYTOCENE_IMPULZY[zak] = 300;
+	AKTUALNI_IMPULZY[zak] = 300;
 	prihlaseny_cip_impulzy = 150;
 
 	printf("\n|--------|---------|\n");
@@ -598,9 +596,9 @@ int main (void)
 	printf("\n|--------|---------|\n");
 	ZobrazInfoCipSprava(zak);
 	printf("\n|--------|---------|\n\n");
+	SOLENOID_OFF();
+#endif
 
-
-#ifdef COMPILE_AVR
 	while(1) {
 
 		//obsluha vsech timeru az tady misto aby se to delalo v preruseni
@@ -627,22 +625,6 @@ int main (void)
 			PrectiCip();
 		}
 
-	 if ( (uartIncoming) && (uartPos == (sizeof(mirfPacket)-1)) ) //whole packet is received
-	 {
-		 uint8_t res = Mirf.sendPacket((mirfPacket*)&outPacket);
-		 uartIncoming = 0;
-
-		 if (res == 0) //packet was not transmitted
-		 {
-			 USART_Transmit("ER", 2);
-			 awaitingResult = 0;
-		 }
-		 else //packet was sent, we will wait for sendResult and send it to usart
-		 {
-			 awaitingResult = 1;
-		 }
-		}
-		
 		if (uartIncoming == 1)
 		{
 			if (timerUart == 0) uartIncoming = 0; //timeout elapsed
@@ -650,7 +632,7 @@ int main (void)
 			if (uartPos == UART_BUFF_MAX_LEN)
 			{
 				//mame prijaty cely buffer, tak zkontrolujem jestli je to spravny pozadavek (string DATA)
-				if ( (uartBuf[0] == 'D') && (uartBuf[0] == 'A') && (uartBuf[0] == 'T') && (uartBuf[0] == 'A') )
+				if ( (uartBuf[0] == 'D') && (uartBuf[1] == 'A') && (uartBuf[2] == 'T') && (uartBuf[3] == 'A') )
 				{
 					//v bufferu je opravdu pozadavek od pocitace - posleme data
 					PosliDataNaUart();
@@ -668,12 +650,11 @@ int main (void)
 			uint8_t novy_stav = DisplayFrontaPop();
 			//prekreslovat jen jestli je v poli neco jineho nez prazdno
 			//jinak pouzijem minuly screen
-			if (novy_stav == STAV_DISPLEJE_NIC) novy_stav = display_posledni_stav;	
+			if (novy_stav == DISP_STAV_NIC) novy_stav = display_posledni_stav;
 			PrekreslitDisplay(novy_stav);
 			refresh_display = false;
 		}
 	}
-#endif
 
 	SaveData();
 	return(0);
