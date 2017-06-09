fabc2938207fb3a71fecb36c0b393a4fcadd1704
Jan Benes <jan.benes@deutsche-boerse.com>
2017-06-09 11:57:47 +0200

    opravil jsem blbost v otaceni poradi byte.. nastesti vcas. tohle by se za behu spatne odhalovalo

--- pivotoc_main.cpp
+++ pivotoc_main.cpp
@@ -529,10 +529,10 @@ void PrectiCip(void)
 		//cip byl detekovan na 1-wire a jeho data nactena, zkusime ho najit v databazi
 		//ale nejdriv musime otocit vsecky BYTE v poli, protoze ted je LSByte ulozen na pozici 0 a MSB na 7
 		//jenze my to potrebujeme obracene
-		for (uint8_t i = ((OW_ROMCODE_SIZE/2)-1); i >= 0; i--)
+		for (uint8_t i = 0; i < (OW_ROMCODE_SIZE/2); i++)
 		{
 			uint8_t buff;
-			uint8_t opacna_strana_pole = ((OW_ROMCODE_SIZE/2)-1) - i;
+			uint8_t opacna_strana_pole = (OW_ROMCODE_SIZE-1) - i;
 
 			buff = prihlaseny_cip_adresa[opacna_strana_pole];
 			prihlaseny_cip_adresa[opacna_strana_pole] = prihlaseny_cip_adresa[i];
